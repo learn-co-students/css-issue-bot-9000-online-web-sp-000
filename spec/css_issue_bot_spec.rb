cbf792499a8cc6d059e5a1611f1db17e7c8f8fcf
Ryan Mui <reachryanmui@gmail.com>
2020-03-26 02:47:42 +0000

    Automatically backed up by Learn

--- spec/css_issue_bot_spec.rb
+++ spec/css_issue_bot_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'styling' do
     end
 
     it 'corrects the #tasks .completed background declaration' do
-      selector = parsed_css.find_by_selector('#tasks.completed')[0]
+      selector = parsed_css.find_by_selector('#tasks .completed')[0]
       expect(selector).to include("background: #ccc;"), "Missing properly formatted #tasks.completed background declaration"
     end
 
